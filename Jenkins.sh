8f0ebafb2225ad4747766815808d3266a0c78366
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-03-14 20:58:35 +0530

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,9 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+#Ipaddress:8080
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
